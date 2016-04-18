def678d74b0acf91566d5e325d0e2d316269028b
droad <droad@ukr.net>
2016-04-18 13:25:14 +0300

    1

--- src/com/javarush/test/level06/lesson08/task03/Util.java
+++ src/com/javarush/test/level06/lesson08/task03/Util.java
@@ -10,6 +10,12 @@ public class Util
     public static double getDistance(int x1, int y1, int x2, int y2)
     {
         //напишите тут ваш код
+        double res;
+
+        double a=((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+        res=Math.sqrt(a);
+        return res;
+
 
     }
 }
