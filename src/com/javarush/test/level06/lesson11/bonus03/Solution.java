2b2e712111eb8732a0bdecf6144e34e139a38165
Дима <droad@ukr.net>
2016-05-01 03:11:06 +0300

    1

--- src/com/javarush/test/level06/lesson11/bonus03/Solution.java
+++ src/com/javarush/test/level06/lesson11/bonus03/Solution.java
@@ -2,9 +2,12 @@ package com.javarush.test.level06.lesson11.bonus03;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 /* Задача по алгоритмам
-Задача: Написать программу, которая вводит с клавиатуры 5 чисел и выводит их в возрастающем порядке.
+Задача: Написать программу, которая вводит с клавиатуры 5 чисел и
+выводит их в возрастающем порядке.
 Пример ввода:
 3
 2
@@ -26,6 +29,27 @@ public class Solution
         BufferedReader reader  = new BufferedReader(new InputStreamReader(System.in));
 
         //напишите тут ваш код
+        int[] array = new int[5];
+        int posrednik;
 
+        for(int i=0;i<5;i++)
+            array[i]=Integer.parseInt(reader.readLine());
+
+/*        for(int i=0; i<array.length-1;i++)
+        {
+            for (int j = 0; j < array.length - i - 1; j++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    posrednik = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = posrednik;
+                }
+            }
+        } */
+
+        Arrays.sort(array);
+        for(int i=0; i<array.length;i++)
+            System.out.println(array[i]);
     }
 }
