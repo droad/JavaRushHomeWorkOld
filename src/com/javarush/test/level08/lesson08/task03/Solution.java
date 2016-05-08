502b4fe588a6b3157a782c6baa296ff600191b9a
Дима <droad@ukr.net>
2016-05-08 17:01:40 +0300

    1

--- src/com/javarush/test/level08/lesson08/task03/Solution.java
+++ src/com/javarush/test/level08/lesson08/task03/Solution.java
@@ -1,57 +1,45 @@
 package com.javarush.test.level08.lesson08.task03;
 
 
+import java.security.Key;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
-
-
-
 /* Одинаковые имя и фамилия
 Создать словарь (Map<String, String>) занести в него десять записей по принципу «Фамилия» - «Имя».
 Проверить сколько людей имеют совпадающие с заданным имя или фамилию.
 */
-
-public class Solution
-{
-    public static Map<String, String> createMap()
-    {
-        //напишите тут ваш код
-        Map<String, String> map = new HashMap<>();
-        map.put("Gf","Ga");
-        map.put("Gf","Fa");
-        map.put("Fg","Da");
-        map.put("Tr","Da");
-        map.put("Sd","Fa");
-        map.put("Sc","Ff");
-        map.put("Sd","Yy");
-        map.put("Drt","Yy");
-        map.put("Sa","Fa");
-        map.put("Sa","Fa");
-
+public class Solution {
+    public static HashMap<String, String> createMap() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("Griffin", "Djustin");
+        map.put("Dior", "Peter");
+        map.put("Dwayn", "Djon");
+        map.put("Sarkisyan", "Lois");
+        map.put("Jackson", "Mikl");
+        map.put("Dimond", "Lessy");
+        map.put("Putin", "Vladimir");
+        map.put("Poroshenko", "Petr");
+        map.put("Girinivskii", "Vladimir");
+        map.put("Balbesovi4", "Balbes");
         return map;
-
     }
-
-    public static int getCountTheSameFirstName(HashMap<String, String> map, String name)
-    {
-        //напишите тут ваш код
-        int n=0;
-        for (Map.Entry<String, String> stringStringEntry : map.entrySet())
-        {
-            if (stringStringEntry.getValue().equals(name)) n++;
-
+    public static int getCountTheSameFirstName(HashMap<String, String> map, String name) {
+        int a = 0;
+                Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getValue().equals(name)) a++;
         }
-        return n;
-
+        return a;
     }
+    public static int getCountTheSameLastName(HashMap<String, String> map, String familiya) {
+        int a = 0;
 
-    public static int getCountTheSameLastName(HashMap<String, String> map, String lastName)
-    {
-        //напишите тут ваш код
-        int n=0;
-        for (Map.Entry<String, String> stringStringEntry : map.entrySet())
-            if (stringStringEntry.getKey().equals(lastName)) n++;
-        return n;
-
+        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getKey().equals(familiya)) a++;
+        }
+        return a;
     }
 }
--- src/com/javarush/test/level08/lesson08/task04/Solution.java
+++ src/com/javarush/test/level08/lesson08/task04/Solution.java
@@ -2,6 +2,8 @@ package com.javarush.test.level08.lesson08.task04;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /* Удалить всех людей, родившихся летом
 Создать словарь (Map<String, Date>) и занести в него десять записей по принципу: «фамилия» - «дата рождения».
@@ -12,19 +14,19 @@ public class Solution
 {
     public static HashMap<String, Date> createMap()
     {
-        HashMap<String, Date> map = new HashMap<>();
+        HashMap<String, Date> map = new HashMap<String, Date>();
         map.put("Stallone", new Date("JUNE 1 1980"));
+        map.put("Stallone2", new Date("JUNE 1 1980"));
+        map.put("Stallone3", new Date("JUNE 1 1980"));
+        map.put("Stallone4", new Date("JUNE 1 1980"));
+        map.put("Stallone5", new Date("JUNE 1 1980"));
+        map.put("Stallone6", new Date("JUNE 1 1980"));
+        map.put("Stallone7", new Date("JUNE 1 1980"));
+        map.put("Stallone8", new Date("MAY 1 1980"));
+        map.put("Stallone9", new Date("JUNE 1 1980"));
+        map.put("Stallone10", new Date("AUG 1 1980"));
 
         //напишите тут ваш код
-        map.put("Vasya", new Date("MAY 1 1980"));
-        map.put("Dima", new Date("FEB 1 1980"));
-        map.put("Olga", new Date("SEP 30 1980"));
-        map.put("Stallone", new Date("APR 1 1980"));
-        map.put("Denis", new Date("JULE 7 1980"));
-        map.put("Bobby", new Date("AUG 20 1980"));
-        map.put("Stallone", new Date("MAY 1 1980"));
-        map.put("Stallone", new Date("JUNE 1 1980"));
-        map.put("Stallone", new Date("JUNE 1 1980"));
         return map;
 
     }
@@ -32,6 +34,16 @@ public class Solution
     public static void removeAllSummerPeople(HashMap<String, Date> map)
     {
         //напишите тут ваш код
+        Iterator<Map.Entry<String, Date>> iterator=map.entrySet().iterator();
+        Date date;
+        while (iterator.hasNext())
+        {
+            date = iterator.next().getValue();
+            if ((date.getMonth()>4)&(date.getMonth()<8))
+                iterator.remove();
+        }
+
+
 
 
 
