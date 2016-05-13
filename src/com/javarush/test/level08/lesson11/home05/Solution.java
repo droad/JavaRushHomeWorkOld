66f19e353a721c187c3fb0ead2271e5ccf9504c0
Dmitry <droad@ukr.net>
2016-05-13 23:51:36 +0300

    1

--- src/com/javarush/test/level08/lesson11/home05/Solution.java
+++ src/com/javarush/test/level08/lesson11/home05/Solution.java
@@ -16,7 +16,7 @@ import java.io.InputStreamReader;
   Мама     Мыла Раму.
 */
 
-public class Solution
+/*public class Solution
 {
     public static void main(String[] args) throws IOException
     {
@@ -36,4 +36,28 @@ public class Solution
     }
 
 
+} */
+
+
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        String s2 = new String();
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            String ch = String.valueOf(s.charAt(i));
+            if(ch.equals(" "))
+                s2 = s2 + s.toUpperCase().charAt(i)+s.toUpperCase().charAt(++i);
+            else
+                s2=s2+s.charAt(i);
+
+        }
+
+        System.out.println(s2);
+
+    }
 }
